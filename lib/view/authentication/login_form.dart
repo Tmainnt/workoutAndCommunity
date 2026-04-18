072a9f145c0eb132771962d0dcedfc75da9358b5
Tmainnt <gamerthann@gmail.com>
2026-04-18 14:45:30 +0700

    initialize file

---
+++ lib/view/authentication/login_form.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => LogFormState();
+}
+
+class LogFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext contexct) {
+    return Scaffold();
+  }
+}

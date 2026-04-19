598a7f47f77fe73cd70212b5c7d3fda8e5c7ec12
Tmainnt <gamerthann@gmail.com>
2026-04-19 13:57:06 +0700

    add widgetShadow, elevatedButtonAuth and dividerColor

--- lib/theme/widget_color.dart
+++ lib/theme/widget_color.dart
@@ -2,4 +2,10 @@ import "package:flutter/material.dart";
 
 class WidgetColor {
   Color textfieldShadow() => Color.fromARGB(255, 158, 158, 158);
+
+  Color widgetShadow() => Color.fromARGB(255, 158, 158, 158);
+
+  Color elevatedButtonAuth() => Color.fromARGB(255, 124, 139, 253);
+
+  Color dividerColor() => Color.fromARGB(255, 158, 158, 158);
 }

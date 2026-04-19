e99dab40de9ac31be0a1cc6b32c41e1a6034b1aa
Tmainnt <gamerthann@gmail.com>
2026-04-19 10:08:29 +0700

    set home: to login form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:woc/view/authentication/login_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginForm());
   }
 }

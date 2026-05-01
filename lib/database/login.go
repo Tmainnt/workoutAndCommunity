8c2a07a381a0881728968aa13f3d56689789556f
Tmainnt <gamerthann@gmail.com>
2026-05-01 11:13:14 +0700

    change name from queryUser to login

---
+++ lib/database/login.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type loginRequest struct {
+	Req_Email    string `json:"user_email"`
+	Req_Password string `json:"user_pass"`
+}
+
+type User struct {
+	Name     string `json:"user_name"`
+	Password string `json:"-"`
+	Email    string `json:"user_email"`
+	FName    string `json:"first_name"`
+	LName    string `json:"last_name"`
+	Gender   string `json:"user_gender"`
+	DOF      string `json:"date_of_birth"`
+	PhoneNB  string `json:"phone_number"`
+	Role     string `json:"role"`
+}
+
+func loginHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req loginRequest
+		var user User
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "Invalid json", 400)
+			return
+		}
+
+		if req.Req_Email == "" || req.Req_Password == "" {
+			http.Error(w, "Email and password required", 400)
+			return
+		}
+
+		err = db.QueryRow(
+			`SELECT user_name, user_email, user_gender, date_of_birth, phone_number, user_pass, role 
+	 FROM users WHERE user_email=$1`,
+			req.Req_Email,
+		).Scan(
+			&user.Name,
+			&user.Email,
+			&user.Gender,
+			&user.DOF,
+			&user.PhoneNB,
+			&user.Password,
+			&user.Role,
+		)
+
+		if err != nil {
+			if err == sql.ErrNoRows {
+				http.Error(w, "User not found	", 400)
+				return
+			}
+			http.Error(w, "Database Error", 500)
+			return
+		}
+
+		err = bcrypt.CompareHashAndPassword(
+			[]byte(user.Password),
+			[]byte(req.Req_Password))
+
+		if err != nil {
+			http.Error(w, "Wrong Password", 401)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "success",
+			"user":    user,
+		})
+	}
+}

a404ea866d260041fdf6330c5d6d9f99404c9885
Tmainnt <gamerthann@gmail.com>
2026-05-06 14:13:40 +0700

    set home: = LoginForm

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: RegisterForm());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginForm());
   }
 }

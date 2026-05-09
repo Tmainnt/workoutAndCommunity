8cdc55a194135137d277fe5e9fcaee38dfb1814d
Tmainnt <gamerthann@gmail.com>
2026-05-10 01:20:27 +0700

    new method registerButtonAction

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -6,6 +6,7 @@ import "package:provider/provider.dart";
 import "package:woc/provider/user_provider.dart";
 import 'package:woc/view/home_page.dart';
 import "package:woc/model/user.dart";
+import "package:woc/view/authentication/login_form.dart";
 
 class AuthService {
   Future<dynamic> loginButtonAction(
@@ -41,6 +42,68 @@ class AuthService {
       return ScaffoldMessenger.of(
         context,
       ).showSnackBar(SnackBar(content: Text("อีเมลหรือรหัสผ่านไม่ถูกต้อง")));
+    }
+  }
+
+  void registerButtonAction(
+    TextEditingController emailController,
+    TextEditingController nameController,
+    TextEditingController passwordController,
+    String? selectedGender,
+    TextEditingController dayController,
+    TextEditingController monthController,
+    TextEditingController yearController,
+    TextEditingController confirmPasswordController,
+    BuildContext context,
+    TextEditingController phoneNumberController,
+  ) async {
+    if (emailController.text.isNotEmpty &&
+        nameController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty &&
+        selectedGender != null &&
+        (dayController.text.isNotEmpty &&
+            monthController.text.isNotEmpty &&
+            yearController.text.isNotEmpty)) {
+      if (passwordController.text != confirmPasswordController.text) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("รหัสผ่านไม่ตรงกัน")));
+        return;
+      }
+      final url = Uri.parse(
+        "https://kindling-magnifier-late.ngrok-free.dev/register",
+      );
+
+      final String dob =
+          "${dayController.text}/${monthController.text}/${yearController.text}";
+
+      final response = await http.post(
+        url,
+        headers: {"content-type": "application/json"},
+        body: jsonEncode({
+          "user_name": nameController.text,
+          "user_email": emailController.text,
+          "user_pass": passwordController.text,
+          "gender": selectedGender,
+          "date_of_birth": dob,
+          "phone_number": phoneNumberController.text,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginForm()),
+        );
+      } else {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text("สมัครไม่สำเร็จ")));
+      }
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("กรุณากรอกข้อมูลให้ครบ")));
     }
   }
 }

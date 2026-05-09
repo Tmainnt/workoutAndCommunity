c69d2184c87ab9ab7149f30a039411230ee2b42b
Tmainnt <gamerthann@gmail.com>
2026-05-09 13:22:50 +0700

    complete post model

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -7,11 +7,34 @@ class Post {
   DateTime _updteTimestamp;
 
   Post({
-    required this.content,
-    required this.image,
-    required this.postId,
-    required this.userId,
-    required this.cont
-    required this.createTimestamp,
-  });
+    required int postId,
+    required int userId,
+    required String content,
+    required String image,
+    required DateTime cts,
+    required DateTime uts,
+  }) : _postId = postId,
+       _userId = userId,
+       _content = content,
+       _image = image,
+       _createTimestamp = cts,
+       _updteTimestamp = uts;
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      postId: json["post_id"],
+      userId: json["user_id"],
+      content: json["post_content"],
+      image: json["post_image"],
+      cts: json["create_timestamp"],
+      uts: json["update_timestamp"],
+    );
+  }
+
+  int get postID => _postId;
+  int get userID => _userId;
+  String get content => _content;
+  String get image => _image;
+  DateTime get createTimestamp => _createTimestamp;
+  DateTime get updateTimestatmp => _updteTimestamp;
 }

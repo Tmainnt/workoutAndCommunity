5b401043b8b166f287e07fa6808af7f6c0b0f61d
Tmainnt <gamerthann@gmail.com>
2026-05-09 12:23:37 +0700

    update model

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -1,6 +1,17 @@
 class Post {
-  String content;
-  String imageUrl;
+  final int _postId;
+  final int _userId;
+  String _content;
+  String _image;
+  final DateTime _createTimestamp;
+  DateTime _updteTimestamp;
 
-  Post({required this.content, required this.imageUrl});
+  Post({
+    required this.content,
+    required this.image,
+    required this.postId,
+    required this.userId,
+    required this.cont
+    required this.createTimestamp,
+  });
 }

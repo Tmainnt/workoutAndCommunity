7f136644437260bfec59e6b3b6a290f7505f2cf9
Tmainnt <gamerthann@gmail.com>
2026-05-16 12:27:04 +0700

    split method generate token to GenerateAccessToken and GenerateRefreshToken

--- database/auth/jwt.go
+++ database/auth/jwt.go
@@ -8,19 +8,30 @@ import (
 )
 
 type Claims struct {
-	Email string `json:"email"`
-	Role  string `json:"role"`
+	UserID    int    `json:"user_id"`
+	Email     string `json:"email"`
+	Role      string `json:"role"`
+	TokenType string `json:"type"`
 	jwt.RegisteredClaims
 }
 
-var jwtKey = []byte(os.Getenv(""))
+type RefreshClaims struct {
+	UserID    int    `json:"user_id"`
+	Email     string `json:"email"`
+	TokenType string `json:"type"`
+	jwt.RegisteredClaims
+}
 
-func GenerateToken(email string, role string) (string, error) {
-	expirationTime := time.Now().Add(24 * time.Hour)
+var jwtKey = []byte(os.Getenv("qKEOym4Uusvl0Oh1WSwH"))
+
+func GenerateAccessToken(id int, email string, role string) (string, error) {
+	expirationTime := time.Now().Add(15 * time.Minute)
 
 	claims := &Claims{
-		Email: email,
-		Role:  role,
+		UserID:    id,
+		Email:     email,
+		Role:      role,
+		TokenType: "AccessToken",
 		RegisteredClaims: jwt.RegisteredClaims{
 			ExpiresAt: jwt.NewNumericDate(expirationTime),
 		},
@@ -28,4 +39,32 @@ func GenerateToken(email string, role string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(jwtKey)
+}
+
+func GenerateRefreshToken(id int, email string, role string) (string, error) {
+	expirationTime := time.Now().Add(7 * 24 * time.Hour)
+
+	claims := &RefreshClaims{
+		UserID:    id,
+		Email:     email,
+		TokenType: "RefreshToken",
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ParseToken(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, err
 }

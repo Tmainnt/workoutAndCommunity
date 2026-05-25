80a0a3634387704ec18883046ced195bf41f72e1
Tmainnt <gamerthann@gmail.com>
2026-05-25 12:15:15 +0700

    new method check snapshot

--- lib/service/post_service.dart
+++ lib/service/post_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:woc/model/post.dart';
 import 'package:http/http.dart' as http;
 
@@ -14,6 +15,22 @@ class PostService {
     }
 
     final List data = jsonDecode(response.body) as List;
-    return data.map((e) => Post.fromJson(e)).toList();  
+    return data.map((e) => Post.fromJson(e)).toList();
+  }
+
+  dynamic checkHasData(AsyncSnapshot snapshot) {
+    if (snapshot.hasError) {
+      return Center(child: Text('ผิดพลาดในการอ่านข้อมูล ${snapshot.error}'));
+    }
+
+    if (snapshot.connectionState == ConnectionState.waiting) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    if (!snapshot.hasData || snapshot.data == null) {
+      return const Center(child: Text('No data.'));
+    }
+
+    return true;
   }
 }

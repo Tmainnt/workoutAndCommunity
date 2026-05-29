daf842cc0f7cd19f2b64351fdc4eacd47941f59b
Tmainnt <gamerthann@gmail.com>
2026-05-29 11:21:12 +0700

    initialize navbat

---
+++ lib/widget/navbar/bottom_navbar.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+class BottomNavbar extends StatelessWidget {
+  final int index;
+  final Function(int) onTap;
+  List<Object> pages = [];
+
+  const BottomNavbar({super.key, required this.index, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(currentIndex: index, onTap: onTap, items: [BottomNavigationBarItem(icon: Icon(Icons.chat))]);
+  }
+}
